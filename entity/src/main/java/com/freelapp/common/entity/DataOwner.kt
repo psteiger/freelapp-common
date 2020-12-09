ec9f16c5d3a0ebf1c3e1640de5ae29d2cb3a1dbe
psteiger <psteiger@gmail.com>
2020-12-09 15:47:46 -0300

    Adding obligatory location property on Owner

--- entity/src/main/java/com/freelapp/common/entity/DataOwner.kt
+++ entity/src/main/java/com/freelapp/common/entity/DataOwner.kt
@@ -4,5 +4,6 @@ interface DataOwner<Owner, DataType> where Owner : DataOwner<Owner, DataType>,
                                            DataType : Data<DataType> {
 
     val data: List<DataType>
+    val location: Pair<Latitude, Longitude>
     fun clone(newData: List<DataType>): Owner
 }
--- entity/src/main/java/com/freelapp/common/entity/TypeAlias.kt
+++ entity/src/main/java/com/freelapp/common/entity/TypeAlias.kt
@@ -1,3 +1,5 @@
 package com.freelapp.common.entity
 
 typealias Key = String
+typealias Latitude = Double
+typealias Longitude = Double
